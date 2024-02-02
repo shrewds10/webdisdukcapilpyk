f82fc24a64c28d2b864e0bf3e91945035f85cae4
shrewds10 <shrewds10@gmail.com>
2024-02-02 14:48:44 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./app.css"
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Popular from './Components/Popular/Popular'
+import Blog from './Components/Blog/Blog'
+import Offer from './Components/Offers/Offer'
+import About from './Components/About/About'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      {/* <Home/>
+      <Popular/>
+      <Offer/>
+      <About/>
+      <Blog/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <section className='navBarSection'>
+      <div className="header">
+        <div className="logoDiv">
+          <a href="#" className="logo">
+            <h3>
+              <img src={require('../../Assets/Logo Kota Payakumbuh.png')} alt="" />
+              Dinas Kependudukan dan Pencatatan Sipil Kota Payakumbuh
+            </h3>
+          </a>
+        </div>
+
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="" className="navLink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Menu 1</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Menu 2</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Menu 3</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Menu 4</a>
+            </li>
+            </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Navbar
