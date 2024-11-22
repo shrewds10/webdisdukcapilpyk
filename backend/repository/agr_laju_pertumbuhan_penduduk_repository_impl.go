22309dcf0fb07ec689f7a63883e8ad592c1bb08f
shrewds10 <shrewds10@gmail.com>
2024-11-22 15:51:48 +0700

    tird commit

--- backend/repository/agr_laju_pertumbuhan_penduduk_repository_impl.go
+++ backend/repository/agr_laju_pertumbuhan_penduduk_repository_impl.go
@@ -3,12 +3,18 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"go_import/helper"
 	"go_import/model/entity"
 )
 
 type AgrLajuPertumbuhanPendudukRepositoryImpl struct {
 }
 
-func (agrLajuPertumbuhanPendudukRepository AgrLajuPertumbuhanPendudukRepositoryImpl) Create(ctx *context.Context, tx *sql.Tx, agreAgrLajuPertumbuhanPenduduk []entity.AgrLajuPertumbuhanPenduduk){
-	
+func (agrLajuPertumbuhanPendudukRepository AgrLajuPertumbuhanPendudukRepositoryImpl) Create(ctx *context.Context, tx *sql.Tx, agreAgrLajuPertumbuhanPenduduk []entity.AgrLajuPertumbuhanPenduduk) {
+	sqlQuery := "INSERT INTO AGR_LAJU_PERTUMBUHAN_PENDUDUK (KODE, SEMESTER, TAHUN, PRIA, WANITA) VALUES (?,?,?,?,?)"
+
+	for i := 0; i < len(agreAgrLajuPertumbuhanPenduduk); i++ {
+		_, err := tx.ExecContext(ctx, sqlQuery, agrLajuPertumbuhanPendudukRepository[i].KODE, user[i].SEMESTER)
+		helper.PanicIfError(err)
+	}
 }
