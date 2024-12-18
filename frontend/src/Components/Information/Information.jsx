ead483e256257f8e533799fa53680aeaf4e8ab8a
shrewds10 <shrewds10@gmail.com>
2024-12-18 15:58:57 +0700

    commit additional drop down

--- frontend/src/Components/Information/Information.jsx
+++ frontend/src/Components/Information/Information.jsx
@@ -8,9 +8,20 @@ import Pendidikan from "../Aggregate/Pendidikan/Pendidikan"
 import Perkawinan from "../Aggregate/Perkawinan/Perkawinan"
 import PertumbuhanPenduduk from "../Aggregate/PertumbuhanPenduduk/PertumbuhanPenduduk"
 import './information.css'
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 
 const Information = () => {
+  const [semester, setSemester] = useState('1')
+  const [tahun, setTahun] = useState('2024')
+
+  const handleSemesterChange = (event) => {
+    setSemester(event.target.value)
+  }
+  
+  const handleTahunChange = (event) => {
+    setSemester(event.target.value)
+  }
+
   return (
     <div className="mcInformation">
       <div className="information container section">
@@ -21,18 +32,34 @@ const Information = () => {
               <h1> Kota Payakumbuh</h1>
             </div>
           </div>
+          <div className="dropdownContainer">
+            <div className="box">
+              <label>Periode:</label>
+              <select value={semester} onChange={handleSemesterChange}>
+                <option value="1">Semester 1</option>
+                <option value="2">Semester 2</option>
+              </select>
+            </div>
+            <div className="box">
+              <label>Tahun:</label>
+              <select value={tahun} onChange={handleTahunChange}>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+              </select>
+            </div>
+          </div>
           <div className="mainContent">
             <div className="Information">
               <div className="descInformation">
-                <JumlahPenduduk/>
-                <PertumbuhanPenduduk/>
-                <KepadatanPenduduk/>
-                <KelompokUmur/>
-                <Agama/>
-                <Perkawinan/>
-                <Pekerjaan/>
-                <Pendidikan/>
-                <GolonganDarah/>
+                <JumlahPenduduk semester={semester} tahun={tahun}/>
+                <PertumbuhanPenduduk semester={semester} tahun={tahun}/>
+                <KepadatanPenduduk semester={semester} tahun={tahun}/>
+                <KelompokUmur semester={semester} tahun={tahun}/>
+                <Agama semester={semester} tahun={tahun}/>
+                <Perkawinan semester={semester} tahun={tahun}/>
+                <Pekerjaan semester={semester} tahun={tahun}/>
+                <Pendidikan semester={semester} tahun={tahun}/>
+                <GolonganDarah semester={semester} tahun={tahun}/>
               </div>
             </div>
           </div>
